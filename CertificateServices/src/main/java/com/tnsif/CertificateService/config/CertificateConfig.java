36816a0c7551ae91173ecd703461d937e906b27d
DevadharshiniS1209 <devadharshini77777@gmail.com>
2025-11-08 08:39:08 +0530

    Add files via upload

---
+++ CertificateServices/src/main/java/com/tnsif/CertificateService/config/CertificateConfig.java
@@ -0,0 +1,9 @@
+package com.tnsif.CertificateService.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CertificateConfig {
+    // Any additional bean configuration if needed
+}
+
