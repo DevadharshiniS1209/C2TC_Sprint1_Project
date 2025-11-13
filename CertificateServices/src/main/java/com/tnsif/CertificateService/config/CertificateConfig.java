72915a8085e9c7073f0756931e9cac7b338c3656
DevadharshiniS1209 <devadharshini77777@gmail.com>
2025-11-13 12:16:39 +0530

    Delete CertificateServices/src/main/java/com/tnsif/CertificateService/config directory

--- CertificateServices/src/main/java/com/tnsif/CertificateService/config/CertificateConfig.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.CertificateService.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class CertificateConfig {
-    // Any additional bean configuration if needed
-}
-
